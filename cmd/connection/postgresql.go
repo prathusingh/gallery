0c6ae96e5db3f22108e3e3aaff593a5795c62f83
prathusingh <prathusingh21@gmail.com>
2021-12-06 21:57:14 -0500

    feat: Created error.go

--- cmd/connection/postgresql.go
+++ cmd/connection/postgresql.go
@@ -4,12 +4,25 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"net/url"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func NewPostgreSQL() {
-	dbpool, err := pgxpool.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	dsn := url.URL {
+		Scheme: "postgres",
+		User: url.UserPassword("", "")
+		Host: fmt.Sprintf("%s:%s", "", "")
+		Path: ""
+	}
+
+	q := dsn.Query()
+	q.Add("sslMode", "")
+
+	dsn.RawQuery = q.Encode()
+	
+	dbpool, err := pgxpool.Connect(context.Background(), dsn.String())
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
