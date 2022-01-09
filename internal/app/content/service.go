b4be5fd68c61b1a1511b2b21d03febe0aabb20a8
prathusingh <prathusingh21@gmail.com>
2022-01-09 14:31:38 -0500

    fix service

--- internal/app/content/service.go
+++ internal/app/content/service.go
@@ -1,26 +1,36 @@
-package image
+package content
 
 import "context"
 
-
+// Image is used for displaying images
 type Image struct {
-	ID string `json:"id"`
-	Name string `json:"name"`
+	ImageID string `json:"id"`
+	ImageName string `json:"name"`
 }
 
-type Service interface {
-	GetImages(ctx context.Context)([]Image, error)
-	
+// Description is used to describe the image
+type Description struct {
+	DescriptionID string `json:"id"`
+	DescriptionName string `json:"name"`
 }
 
-type imageService struct {
+type content struct {
+	image Image
+	description Description
+}
+
+type service interface {
+	getContent(ctx context.Context, imageID string, descriptionID string)(content, error)
+}
+
+type contentService struct {
 	repository Repository
 }
 
-func NewService(repository Repository) Service {
-	return &imageService{repository}
+func NewService(repository Repository) service {
+	return &contentService{repository}
 }
 
-func (imgServ *imageService) GetImages(ctx context.Context) ([]Image) {
-	return imgServ.repository.GetImages(ctx)
+func (contentServ *contentService) getContent(ctx context.Context) (content, error) {
+	return contentServ.repository.GetImages(ctx)
 }
