21ae820b705c514be18b5fa926f9ef836ea919f4
prathusingh <prathusingh21@gmail.com>
2022-05-01 21:21:23 -0400

    chore: added config files

---
+++ client/web/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  settings: {
+    // This is needed to make eslint happy with name aliases
+    "import/resolver": {
+      "babel-module": {},
+    },
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/no-shadow": ["error"],
+        "no-shadow": "off",
+        "no-undef": "off",
+      },
+    },
+  ],
+}
